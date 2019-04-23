2ca6d113b781464e11feb96be6a59ced0a76811f
Ahad Patel <47653152+Dextro1597@users.noreply.github.com>
2019-04-23 18:14:47 +0530

    Update diffie.py

--- diffie.py
+++ diffie.py
@@ -1,24 +1,24 @@
 sharedPrime = 23 #p
 sharedBase = 5 #q
 
-aliceSecrect = 6
-bobSecrect = 3
+faizalSecrect = 6 #a
+definiteSecrect = 3 #b
 
 print('Publically Shared Variables')
 print('Value of P:-' +str(sharedPrime))
 print('Value of q:-' +str(sharedBase))
 
 
-A = (sharedBase ** aliceSecrect) % sharedPrime
-print('\n Alice Sends over Public channel:', str(A))
+A = (sharedBase ** faizalSecrect) % sharedPrime
+print('\n Faizal Sends over Public channel:', str(A))
 
-B = (sharedBase ** bobSecrect) % sharedPrime
-print('\n Bob Sends over Public Channel:', str(B))
+B = (sharedBase ** definiteSecrect) % sharedPrime
+print('\n Definite Sends over Public Channel:', str(B))
 
 
-A1 = (B ** aliceSecrect) % sharedPrime
-print('\n Alice Shared Secrect:', str(A1))
+A1 = (B ** faizalSecrect) % sharedPrime
+print('\n Faizal Shared Secrect:', str(A1))
 
-B1 = (A ** bobSecrect) % sharedPrime
-print('\n Bob Shared Secrect:', str(B1))
+B1 = (A ** definiteSecrect) % sharedPrime
+print('\n Definite Shared Secrect:', str(B1))
 
